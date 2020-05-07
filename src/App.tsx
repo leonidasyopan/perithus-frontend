15a3e2e9fef7d615dd0359525f9fe3d47bd138a8
Leonidas Yopan <leonidasyopan@gmail.com>
2020-05-07 10:46:20 -0300

    recomeçando p/ eliminar problemas com dependencias

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-import Routes from './routes';
 
 function App() {
-  return (
-    <Router>
-      <Routes />
-    </Router>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
